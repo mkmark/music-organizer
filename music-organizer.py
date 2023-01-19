fa9d28308fa9bdd9ce528362a9ff84f080af9dcf
Mark Ma <mark@mkmark.net>
2023-01-19 09:07:57 +0800

    fix: albumartist as performer

--- music-organizer.py
+++ music-organizer.py
@@ -1,41 +1,59 @@
-"""Organize music from <src> to <dest> in format of <Artist>\\<Album>\\<Title>.<extension>"""
+"""Organize music from <src> to <dst> in format of <Artist>\\<Album>\\<Title>.<extension>"""
 
 import taglib
 from pathlib import Path
 import os
 import re
 import argparse
+import logging
+
 
 def make_string_windows_compatible(_str):
-    _str = re.sub(r'[\/]', '／', _str)
-    _str = re.sub(r'[\\]', '＼', _str)
-    _str = re.sub(r'[\"]', '＂', _str)
-    _str = re.sub(r'[\:]', '：', _str)
-    _str = re.sub(r'[\*]', '＊', _str)
-    _str = re.sub(r'[\?]', '？', _str)
-    _str = re.sub(r'[\<]', '《', _str)
-    _str = re.sub(r'[\>]', '》', _str)
-    _str = re.sub(r'[\|]', '｜', _str)
+    _str = re.sub(r"[\/]", "／", _str)
+    _str = re.sub(r"[\\]", "＼", _str)
+    _str = re.sub(r"[\"]", "＂", _str)
+    _str = re.sub(r"[\:]", "：", _str)
+    _str = re.sub(r"[\*]", "＊", _str)
+    _str = re.sub(r"[\?]", "？", _str)
+    _str = re.sub(r"[\<]", "《", _str)
+    _str = re.sub(r"[\>]", "》", _str)
+    _str = re.sub(r"[\|]", "｜", _str)
+    _str = _str.lstrip(" ")
+    _str = _str.rstrip(" .")
     return _str
+
+
+def make_performer_windows_compatible(_str):
+    _str = re.sub(r"[\/]", ",", _str)
+    _str = re.sub(r"[\\]", ",", _str)
+    _str = re.sub(r"[\"]", "＂", _str)
+    _str = re.sub(r"[\:]", "：", _str)
+    _str = re.sub(r"[\*]", "＊", _str)
+    _str = re.sub(r"[\?]", "？", _str)
+    _str = re.sub(r"[\<]", "《", _str)
+    _str = re.sub(r"[\>]", "》", _str)
+    _str = re.sub(r"[\|]", "｜", _str)
+    return _str
+
 
 def main():
     # parse args
-    parser = argparse.ArgumentParser(description="""
-    This script organize music from <src> to <dest> in format of <Artist>\\<Album>\\<Title>.<extension>
-    """)
-    parser.add_argument("-s --src", \
-                        dest = "SRC", \
-                        help = "source direcotry path", \
-                        required = True)
-    parser.add_argument("-d --dest", \
-                        dest = "DEST", \
-                        help = "destination direcotry path", \
-                        required = True)
+    parser = argparse.ArgumentParser(
+        description="""
+    This script organize music from <src> to <dst> in format of <Artist>\\<Album>\\<Title>.<extension>
+    """
+    )
+    parser.add_argument(
+        "-s --src", dest="SRC", help="source direcotry path", required=True
+    )
+    parser.add_argument(
+        "-d --dst", dest="DST", help="destination direcotry path", required=True
+    )
 
     # define source and destination
     args = parser.parse_args()
     src_base_path = args.SRC
-    dest_base_path = args.DEST
+    dst_base_path = args.DST
 
     # get all files from source
     mp3_paths = list(Path(src_base_path).rglob("*.[mM][pP]3"))
@@ -45,31 +63,51 @@ def main():
     for song_path in song_paths:
         # get tags info
         tagged_song = taglib.File(str(song_path))
-        artist = ",".join(tagged_song.tags["ARTIST"])
-        album = tagged_song.tags["ALBUM"][0]
-        title = tagged_song.tags["TITLE"][0]
+        try:
+            artists = ",".join(tagged_song.tags["ARTIST"])
+            album_artists = ",".join(tagged_song.tags["ALBUMARTIST"])
+            if album_artists:
+                performer = artists
+            else:
+                performer = album_artists
+            album = tagged_song.tags["ALBUM"][0]
+            title = tagged_song.tags["TITLE"][0]
+        except:
+            logging.error("missing info: %s", str(song_path))
+            tagged_song.close()
+            continue
         tagged_song.close()
 
         # decompose file name
         filename = os.path.basename(song_path)
-        extension = filename.split('.')[-1]
-        filepath_without_extension = '.'.join(str(song_path).split('.')[0:-1])
+        extension = filename.split(".")[-1]
+        filepath_without_extension = ".".join(str(song_path).split(".")[0:-1])
 
         # make string windows compatible
-        artist = make_string_windows_compatible(artist)
+        performer = make_performer_windows_compatible(performer)
         album = make_string_windows_compatible(album)
         title = make_string_windows_compatible(title)
 
         # move music
-        if not os.path.exists(dest_base_path + artist + '\\'):
-            os.mkdir(dest_base_path + artist + '\\')
-        if not os.path.exists(dest_base_path + artist + '\\' + album + '\\'):
-            os.mkdir(dest_base_path + artist + '\\' + album + '\\')
-        os.rename(str(song_path), (dest_base_path + artist + '\\' + album + '\\' + title + '.' + extension))
+        if not os.path.exists(dst_base_path + performer + "\\"):
+            os.mkdir(dst_base_path + performer + "\\")
+        if not os.path.exists(dst_base_path + performer + "\\" + album + "\\"):
+            os.mkdir(dst_base_path + performer + "\\" + album + "\\")
+        song_dest_path = (
+            dst_base_path + performer + "\\" + album + "\\" + title + "." + extension
+        )
+        try:
+            os.rename(str(song_path), song_dest_path)
+            # move lrc
+            if os.path.exists(filepath_without_extension + ".lrc"):
+                os.rename(
+                    filepath_without_extension + ".lrc",
+                    (dst_base_path + performer + "\\" + album + "\\" + title + ".lrc"),
+                )
+        except:
+            logging.warning("Files already exists: %s", song_dest_path)
+            continue
 
-        # move lrc
-        if os.path.exists(filepath_without_extension + '.lrc'):
-            os.rename(filepath_without_extension + '.lrc', (dest_base_path + artist + '\\' + album + '\\' + title + '.' + '.lrc'))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
